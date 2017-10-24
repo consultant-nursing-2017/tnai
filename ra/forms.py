6b9d14eb773ce01752512a35151b5163c8794b08
S. Anand <consultant.nursing.2017@gmail.com>
2017-10-24 15:25:51 +0530

    Commit leftover ra/forms.py

---
+++ ra/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.forms import ModelForm
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import datetime #for checking renewal date range.
+from django.template import Context
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from candidate.models import ProfessionalQualifications, EligibilityTests
+    
+class FilterForm(forms.Form):
+    gender = forms.ChoiceField([(None, '--- Either ---'), ('Male', 'Male'), ('Female', 'Female'), ])
+    COURSE_CHOICES = [(None, '--- Any ---')]
+    COURSES = ProfessionalQualifications.course_choices()
+    for course in COURSES:
+        COURSE_CHOICES.append((course, course))
+    professional_qualifications = forms.ChoiceField(COURSE_CHOICES)
+    experience = forms.CharField()
+    ELIGIBILITY_TESTS_CHOICES = [(None, '--- Any ---')] + list(EligibilityTests.eligibility_tests_choices())
+    eligibility_tests = forms.ChoiceField(ELIGIBILITY_TESTS_CHOICES)
+
+class SignupForm(UserCreationForm):
+#    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+        labels = {'username': 'Email address (will also serve as your username)'}
