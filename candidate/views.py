4b9b10cf4e738cac26eb16639143ecb81ffe5083
S. Anand <consultant.nursing.2017@gmail.com>
2017-10-24 20:10:42 +0530

    Fixed password reset process
    - Need to update the email for the old candidates.

--- candidate/views.py
+++ candidate/views.py
@@ -502,6 +502,7 @@ def signup(request):
         if form.is_valid():
             user = form.save(commit=False)
             user.is_active = False
+            user.email = user.username
             user.save()
             current_site = get_current_site(request)
             message = render_to_string('candidate/acc_active_email.html', {
