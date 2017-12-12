02df394a3ef9ead28f92fd65f36d129334b36ffb
S. Anand <consultant.nursing.2017@gmail.com>
2017-12-12 16:28:28 +0530

    Add gender to advertisement form.

--- employer/models.py
+++ employer/models.py
@@ -61,13 +61,14 @@ class Advertisement(models.Model):
             ("None", "None"),
     )
     GENDER_CHOICES = (
+            ("Any", "Any"),
             ("Male", "Male"),
             ("Female", "Female")
     )
     employer_advert = models.ForeignKey(Employer, on_delete=models.CASCADE, related_name='employer_advert', blank=False, null=True)
     job_role = models.CharField(max_length=200, blank=False, default="Nurse")
     closing_date = models.DateField(blank=False, default=timezone.now)
-#    gender = models.CharField(max_length=200, choices=GENDER_CHOICES, default="Male")
+    gender = models.CharField(max_length=200, choices=GENDER_CHOICES, default="Any", blank=False)
     number_of_vacancies = models.IntegerField(blank=False, default=5)
     educational_qualifications = models.CharField(max_length=500, blank=False, default="ANM required")
     eligibility_tests = models.CharField(max_length=500, blank=False, choices=EligibilityTests.eligibility_tests_choices(), default="HAAD")
